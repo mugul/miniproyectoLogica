a169f2462cb4d028efd77e4453637362e7090dac
mugul <mugul@github.com>
2017-03-29 18:11:09 -0400

    Modificado agregarteorema para que funcione para agregar un teorema

--- src/main/java/com/howtodoinjava/lambdacalculo/Main.java
+++ src/main/java/com/howtodoinjava/lambdacalculo/Main.java
@@ -4,18 +4,13 @@
  */
 package com.howtodoinjava.lambdacalculo;
 
-
 import java.util.ArrayList;
-
 
 /**
  *
  * @author federico
  */
 public class Main {
-    
-    
-    
 
     public static void main(String args[]) {
         ArrayList<Object> lisObj = new ArrayList<Object>();
@@ -28,18 +23,17 @@ public class Main {
         Term t1 = mk.makeTerm("(!q == true) ==> (p== q)");
 
 
-         Term v = mk.makeTerm("p \\/ q == (q ==> true)");
-         Term v1 = mk.makeTerm("r");
-         Term a = mk.makeApp("p","r");
-         
-        
+        Term v = mk.makeTerm("p \\/ q == (q ==> true)");
+        Term v1 = mk.makeTerm("r");
+        Term a = mk.makeApp("p", "r");
+
+
         System.out.println("-----------------------------");
         System.out.println(v.toStringInf());
         System.out.println("+++++++++++++++++++++++++++++");
         System.out.println("+++++++++++++++++++++++++++++");
         System.out.println("+++++++++++++++++++++++++++++");
-        
-        
-        
+
 
     }
+}
